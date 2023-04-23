21676f5e51b1a99d1b3911775ad9893bfe24b751
Tulasi Latha Gangadhari <65495688+TulasiLatha@users.noreply.github.com>
2023-04-24 03:07:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function helloworld(){
+	setTimeout((resolve,reject)=>{
+		reslove("Hello, world!")
+	},1000)
+}
+const ouputElemnt=document.getElementById('output')
+helloworld.then((message)=>{
+	ouputElemnt.innerText=message;
+}).catch((error) => {
+  console.error(error);
+});
